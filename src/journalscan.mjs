a5f2e452db0126313c7a01e9705ecf81f1615254
Simon Young <sjlyoung58@gmail.com>
2020-05-16 22:23:06 +0100

    added scripts, WIP

--- src/journalscan.mjs
+++ src/journalscan.mjs
@@ -11,3 +11,23 @@ const output = fs.readFileSync('testjournals/Cargo.json');
 
 console.log(config.db.user);
 console.log(output.toString());
+
+fs.readdir('./testjournals/', function(err, files) {
+    if (err) {
+      console.log("Error getting directory information.")
+    } else {
+      files.forEach(function(file) {
+        const nameParts = file.split('.');
+        if (nameParts[0] == 'Journal') {
+            console.log("its a journal")            
+        } else {
+            console.log("rejected")                        
+        }
+        processJournal(nameParts);
+      })
+    }
+  })
+
+function processJournal(nameParts) {
+    console.log(nameParts);
+}
